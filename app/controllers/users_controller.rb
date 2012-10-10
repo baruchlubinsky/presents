f53a8b48260e6bc410294e73b0af94cdb3b5c76d
Baruch Lubinsky <baruchlubinsky@gmail.com>
2012-10-10 17:20:13 +0200

    email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user.valid?
       if @user.save
         session[:user_id] = @user.id
-        message = SignupConfirmation.welcome(params[:user][:first_name], params[:user][:password])
+        message = SignupConfirmation.welcome(@user, params[:user][:password])
         message.deliver
         if session[:return_path].nil?
           redirect_to home_path
