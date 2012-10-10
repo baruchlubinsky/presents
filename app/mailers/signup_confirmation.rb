4d19e44cf47771c4423bf77c3ef8985dcf4876ee
Baruch Lubinsky <baruchlubinsky@gmail.com>
2012-10-10 17:15:40 +0200

    email

--- app/mailers/signup_confirmation.rb
+++ app/mailers/signup_confirmation.rb
@@ -2,7 +2,7 @@ class SignupConfirmation < ActionMailer::Base
   default from: "Raj <raj@presentsinthepost.co.za>"
   
   def welcome(user, password)
-    @recipient = user.name
+    @user = user
     @password = password
     mail :to => user.email, :subject => "Welcome to Presents in the Post"
   end
