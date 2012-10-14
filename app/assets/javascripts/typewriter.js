21474bcaf09caa6a7e9dd9d647a7c46489a49e23
Baruch Lubinsky <baruchlubinsky@gmail.com>
2012-10-14 20:43:50 +0200

    analytics

--- app/assets/javascripts/typewriter.js
+++ app/assets/javascripts/typewriter.js
@@ -2,14 +2,17 @@ $(document).ready(function (){
 	$('textarea').keypress(playKeyPress);
 });
 
+
 keySound = new Audio('https://s3-eu-west-1.amazonaws.com/presentsinthepost/sounds/key.wav');
 keySound.volume = 0.4;
 
+
+
 function playKeyPress()
 {
-	if(!(keySound.currentTime == 0))
-	{
-		keySound.currentTime = 0;
-	}
-	keySound.play();
+		if(!(keySound.currentTime == 0))
+		{
+			keySound.currentTime = 0;
+		}
+		keySound.play();
 }
