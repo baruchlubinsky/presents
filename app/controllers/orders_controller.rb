95fb59606b1cfe37653e40e0a4241b9f75bac527
Baruch Lubinsky <baruchlubinsky@gmail.com>
2012-12-13 23:30:08 +0200

    fixed order list

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,12 @@ class OrdersController < ApplicationController
       @from_date = DateTime.new(params[:from]['date(1i)'].to_i, params[:from]['date(2i)'].to_i, params[:from]['date(3i)'].to_i).to_time
     else
       present = Present.where(:online => true).asc(:created_at).first
-      @from_date = present.created_at
+      if present?
+        @from_date = present.created_at
+      else
+        present = Present.where(:online => false).desc(:created_at).first
+        @from_date = present.created_at
+      end
     end
     if params[:to]
       @to_date = DateTime.new(params[:to]['date(1i)'].to_i, params[:to]['date(2i)'].to_i, params[:to]['date(3i)'].to_i).to_time
